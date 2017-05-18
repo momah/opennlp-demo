76c894f8041411b117afe8e73d956e9bf6245a19
mohammed <simotenerife@hotmail.com>
2017-05-18 03:16:59 +0100

    Más pruebas unitarias
    Más pruebas unitarias

--- src/main/java/org/fogbeam/example/opennlp/test/TestTokenizer.java
+++ src/main/java/org/fogbeam/example/opennlp/test/TestTokenizer.java
@@ -26,4 +26,47 @@ public class TestTokenizer {
     }
   }
 
+
+  @Test
+  public void ficheroVacio() throws Exception {
+  //Comprobar si el fichero no está vacio
+   String strOriginal=TokenizerMain.fichero_a_cadena("test/texto_02.txt");
+   boolean same=false;
+
+   if(!strOriginal.isEmpty())
+      same=true;
+
+  		assertTrue(same == true);
+  }
+
+  @Test
+  public void mismaCadena() throws Exception{
+  @
+  //Comprobar que quincide contenido del fichero texto_01
+  	String strOriginal=TokenizerMain.fichero_a_cadena("test/texto_01.txt");
+  	boolean same=false;
+
+  	String byText="One can resist the invasión of an army but one cannot resist the invasion of ideas. Victor Hugo";
+
+		if(strOriginal.equals(byText))
+  		same=true;
+
+		assertTrue(same == true);
+	}
+
+  @Test
+  public void mismaCadena() throws Exception {
+    @
+  //Comprobar que no quincide contenido del fichero texto_01
+    String strOriginal=TokenizerMain.fichero_a_cadena("test/texto_01.txt");
+    boolean same=false;
+
+    String byText="In a village of La Mancha, the name of which I have no desire to call to mind.";
+
+    if(strOriginal.equals(byText))
+      same=true;
+
+    assertFalse(same == true);
+  }
+
 }
